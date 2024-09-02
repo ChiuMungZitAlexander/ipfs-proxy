2b9618db13bd86d50a99f031d02cff679f0853f0
AlexanderZhao <zhaomengzhe@gmail.com>
2024-09-02 13:00:49 +0800

    feat(): project init

---
+++ src/app/route.ts
@@ -0,0 +1,35 @@
+import { type NextRequest, NextResponse } from "next/server";
+import axios from "axios";
+
+export const maxDuration = 15;
+
+export const GET = async (request: NextRequest) => {
+  const searchParams = request.nextUrl.searchParams;
+  let ipfs = searchParams.get("ipfs");
+
+  if (!ipfs || !/ipfs:\/\//.test(ipfs)) {
+    return new Response("Invalid ipfs", {
+      status: 400,
+    });
+  }
+
+  ipfs = decodeURIComponent(ipfs);
+
+  try {
+    const response = await axios({
+      url: ipfs.replace(/^ipfs:\/\//, "https://ipfs.io/ipfs/"),
+      method: "GET",
+      responseType: "stream",
+    });
+
+    return new NextResponse(response.data, {
+      status: 200,
+      headers: {
+        "Content-Type": response.headers["content-type"],
+      },
+    });
+  } catch (error) {
+    console.error("Error fetching IPFS content:", error);
+    return new NextResponse("Failed to fetch image", { status: 500 });
+  }
+};
